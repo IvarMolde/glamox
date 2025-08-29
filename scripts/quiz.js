1148ca8dcca9066de1405757b44ef10ccca7adf2
Ivar <156409852+IvarMolde@users.noreply.github.com>
2025-08-29 08:17:39 +0200

    Update quiz.js

--- scripts/quiz.js
+++ scripts/quiz.js
@@ -479,3 +479,16 @@ function setupDragAndDrop() {
     });
   });
 }
+function getDragAfterElement(container, y) {
+  const candidates = [...container.querySelectorAll(".sortable-item:not(.dragging)")];
+  return candidates.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y - box.top - box.height / 2;
+    if (offset < 0 && offset > closest.offset) {
+      return { offset, element: child };
+    } else {
+      return closest;
+    }
+  }, { offset: Number.NEGATIVE_INFINITY, element: null }).element;
+}
+
